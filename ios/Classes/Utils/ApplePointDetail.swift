eaa707dc11542264a09af9b546a49d23bd410bd3
lh900519 <lh900519@hotmail.com>
2026-01-15 23:46:56 +0800

    formatter

--- ios/Classes/Utils/ApplePointDetail.swift
+++ ios/Classes/Utils/ApplePointDetail.swift
@@ -26,29 +26,26 @@ class ApplePointDetail {
                 return
             }
 
-            // 📍 address 子对象（与 MKMapItem.searchRegion 完全一致）
-            let street = [
-                placemark.subThoroughfare,
-                placemark.thoroughfare,
-            ]
-            .compactMap { $0 }
-            .joined(separator: " ")
-
-            // 📦 顶层 POI 数据（字段与 searchRegion 对齐）
             var detailData: [String: Any] = [
-                "latitude": coordinate.latitude,
-                "longitude": coordinate.longitude,
                 "name": placemark.name ?? "",
-
-                "street": street,
-                "city": placemark.locality ?? "",
-                "district": placemark.subLocality ?? "",
-                "state": placemark.administrativeArea ?? "",
-                "subState": placemark.subAdministrativeArea ?? "",
-                "postalCode": placemark.postalCode ?? "",
-                "country": placemark.country ?? "",
-                "countryCode": placemark.isoCountryCode ?? "",
             ]
+            if let coord = placemark.location?.coordinate {
+                detailData["latitude"] = coord.latitude
+                detailData["longitude"] = coord.longitude
+            }
+
+            // POI 地址
+            detailData["locality"] = placemark.locality ?? ""
+            detailData["subLocality"] = placemark.subLocality
+            detailData["administrativeArea"] = placemark.administrativeArea
+            detailData["subAdministrativeArea"] = placemark.subAdministrativeArea
+            detailData["postalCode"] = placemark.postalCode
+            detailData["country"] = placemark.country
+            detailData["isoCountryCode"] = placemark.isoCountryCode
+
+            // POI 街道
+            detailData["subThoroughfare"] = placemark.subThoroughfare
+            detailData["thoroughfare"] = placemark.thoroughfare
 
             // ⏰ 时区（与 MKMapItem.timeZone 对齐）
             if let timeZone = placemark.timeZone {
@@ -88,56 +85,35 @@ class ApplePointDetail {
                 completion([])
                 return
             }
-            
+
             for item in response.mapItems {
                 var detailData: [String: Any] = [
                     "name": item.name ?? "",
                     "url": item.url?.absoluteString ?? "",
                 ]
-                
-              
-//                // iOS 26
-//                if #available(iOS 26.0, *) {
-//                    let location = item.location
-//                    detailData["latitude"] = location.coordinate.latitude
-//                    detailData["longitude"] = location.coordinate.longitude
-//
-//                    if let addressRepresentations = item.addressRepresentations,
-//                       let address = item.address {
-//                        // POI 地址
-//                        detailData["fullAddress"] = address.fullAddress
-//                        detailData["shortAddress"] = address.shortAddress ?? ""
-//
-//                        detailData["city"] = addressRepresentations.cityName ?? ""
-//                        detailData["cityWithContext"] = addressRepresentations.cityWithContext ?? ""
-//                        detailData["regionName"] = addressRepresentations.regionName ?? ""
-//
-//                    }
-//
-//                } else {
-                    let placemark = item.placemark
-                    let coord = placemark.coordinate
-                    detailData["latitude"] = coord.latitude
-                    detailData["longitude"] = coord.longitude
 
-                    // POI 地址
-                    detailData["locality"] = placemark.locality ?? ""
-                    detailData["subLocality"] = placemark.subLocality
-                    detailData["administrativeArea"] = placemark.administrativeArea
-                    detailData["subAdministrativeArea"] = placemark.subAdministrativeArea
-                    detailData["postalCode"] = placemark.postalCode
-                    detailData["country"] = placemark.country
-                    detailData["isoCountryCode"] = placemark.isoCountryCode
+                let placemark = item.placemark
+                let coord = placemark.coordinate
+                detailData["latitude"] = coord.latitude
+                detailData["longitude"] = coord.longitude
 
-                    // POI 街道
-                    detailData["subThoroughfare"] = placemark.subThoroughfare
-                    detailData["thoroughfare"] = placemark.thoroughfare
+                // POI 地址
+                detailData["locality"] = placemark.locality ?? ""
+                detailData["subLocality"] = placemark.subLocality
+                detailData["administrativeArea"] = placemark.administrativeArea
+                detailData["subAdministrativeArea"] = placemark.subAdministrativeArea
+                detailData["postalCode"] = placemark.postalCode
+                detailData["country"] = placemark.country
+                detailData["isoCountryCode"] = placemark.isoCountryCode
 
-                    // 兴趣点
-                    if let areas = placemark.areasOfInterest, !areas.isEmpty {
-                        detailData["areasOfInterest"] = areas
-                    }
-//                }
+                // POI 街道
+                detailData["subThoroughfare"] = placemark.subThoroughfare
+                detailData["thoroughfare"] = placemark.thoroughfare
+
+                // 兴趣点
+                if let areas = placemark.areasOfInterest, !areas.isEmpty {
+                    detailData["areasOfInterest"] = areas
+                }
 
                 // POI 分类
                 if let pointOfInterestCategory = item.pointOfInterestCategory {
